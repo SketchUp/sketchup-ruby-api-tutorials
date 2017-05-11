08b111930e631e66f7c1d03f7de706ee07c7196d
Dominic Meiser <dominic@sketchup.com>
2017-05-11 16:40:30 -0600

    Minor edits in tool tutorial.
    Comment changes only.

--- tutorials/02_custom_tool/tut_custom_tool/main.rb
+++ tutorials/02_custom_tool/tut_custom_tool/main.rb
@@ -6,8 +6,8 @@ require 'sketchup.rb'
 module Examples
   module CustomTool
 
-    # Custom tools are creating a class which respond to various callback
-    # methods from SketchUp.
+    # Custom tools create a class which responds to various callback methods
+    # from SketchUp.
     class LineTool
 
       # The `actvate` method is called whenever the tool is activated. This is
@@ -17,29 +17,29 @@ module Examples
       # place to set up the tool.
       def activate
 
-        # We will need to sample 3d points from the model as the user interact
+        # We will need to sample 3d points from the model as the user interacts
         # with the tool and the model. For this we use InputPoint which also
-        # add some inference-magic.
+        # adds some inference-magic.
         # We need to sample the 3d point under the mouse cursor and keep a
         # reference of what the user clicks on.
         @mouse_ip = Sketchup::InputPoint.new
         @picked_first_ip = Sketchup::InputPoint.new
 
-        # We make sure to call out utility method that updates the statusbar
-        # with instructions on how to use the tool.
+        # We make sure to call our utility method update_ui that updates
+        # the statusbar with instructions on how to use the tool.
         update_ui
       end
 
-      # This is called when the user switch to a new tool. It's recommended to
-      # always call view.invalidate in order to make sure we clear out any
-      # custom drawings done to the viewport. Otherwise it might linger around
-      # for a moment and confuse the user.
+      # This is called when the user switches to a different tool. It's
+      # recommended to always call view.invalidate in order to make sure we
+      # clear out any custom drawings done to the viewport. Otherwise these
+      # drawings might linger around for a moment confusing the user.
       def deactivate(view)
         view.invalidate
       end
 
       # Tools can be temporarily suspended and resumed. One example of this is
-      # when the user use the Orbit tool by pressing the middle mouse button.
+      # when the user uses the Orbit tool by pressing the middle mouse button.
       # In order to make sure we update our statusbar text and custom viewport
       # drawing we need to do that here.
       def resume(view)
@@ -62,19 +62,18 @@ module Examples
       def onMouseMove(flags, x, y, view)
         # We want to sample the 3d point under the cursor as the user moves it.
         if picked_first_point?
-          # When the user have picked a start point we use that while picking in
-          # order for SketchUp to do it's inference magic.
-          # Note that if you want to allow the user to lock inference you need
-          # to implement `view.lock_inference`. This will be described in a
-          # later tutorial.
+          # When the user picks a start point we use that while picking in
+          # order for SketchUp to do it's inference magic. Note that if you
+          # want to allow the user to lock inferencing you need to implement
+          # `view.lock_inference`. This will be described in a later tutorial.
           @mouse_ip.pick(view, x, y, @picked_first_ip)
         else
-          # When the user haven't picked a start point yet we just use the
+          # When the user hasn't picked a start point yet we just use the
           # x and y coordinates of the cursor.
           @mouse_ip.pick(view, x, y)
         end
-        # Here we let SketchUp display it's hints to have it's inferensing,
-        # similar to how the native tools do it.
+        # Here we let SketchUp display its inferencing similar to how the
+        # native tools do it.
         view.tooltip = @mouse_ip.tooltip if @mouse_ip.valid?
         # Lastly we want to ensure we update the view.
         view.invalidate
@@ -83,7 +82,7 @@ module Examples
       # When the user clicks in the viewport we want to create edges based on
       # the input points we have collected.
       def onLButtonDown(flags, x, y, view)
-        # When the user have picked a start point and then picks another point
+        # When the user has picked a start point and then picks another point
         # we create an edge and try to create new faces from that edge.
         num_new_faces = 0
         if picked_first_point?
@@ -110,15 +109,15 @@ module Examples
       CURSOR_PENCIL = 632
       def onSetCursor
         # Note that `onSetCursor` is called frequently so you should not do much
-        # work here. At most you switch between different cursor representing
+        # work here. At most you switch between different cursors representing
         # the state of the tool.
         UI.set_cursor(CURSOR_PENCIL)
       end
 
       # The `draw` method is called every time SketchUp updates the viewport.
-      # You should take care to do as little work in this method. If you need to
-      # calculate things to draw it its best to cache the data in order to get
-      # better frame rate.
+      # You should take care to do as little work in this method as possible.
+      # If you need to calculate things to draw it is best to cache the data in
+      # order to get better frame rates.
       def draw(view)
         draw_preview(view)
         @mouse_ip.draw(view) if @mouse_ip.display?
@@ -127,7 +126,7 @@ module Examples
       # When you use `view.draw` and draw things outside the boundingbox of
       # the existing model geometry you will see that things get clipped.
       # In order to make sure everything you draw is visible you must return
-      # a boundingbox here which define the 3d model space you draw to.
+      # a boundingbox here which defines the 3d model space you draw to.
       def getExtents
         bb = Geom::BoundingBox.new
         bb.add(picked_points)
@@ -135,7 +134,7 @@ module Examples
       end
 
       # In this example we put all the logic in the tool class itself. For more
-      # complex tools you probably want to move that logic into it's own class
+      # complex tools you probably want to move that logic into its own class
       # in order to reduce complexity. If you are familiar with the MVC pattern
       # then consider a tool class a controller - you want to keep it short and
       # simple.
@@ -187,9 +186,9 @@ module Examples
 
 
     # A utility method to activate the tool. This is defined here for easy
-    # reuse as well as making it easier to debug while developing. If this
-    # code was directly in the `add_item` block and you needed to make changes
-    # to how you activate the tool then it would not take effect until you
+    # reuse as well as making it easier to debug while developing. If this code
+    # was directly in the `add_item` block and you needed to make changes to
+    # how you activate the tool then it would not take effect until you
     # restarted SketchUp due to the load guard.
     def self.activate_line_tool
       Sketchup.active_model.select_tool(LineTool.new)
